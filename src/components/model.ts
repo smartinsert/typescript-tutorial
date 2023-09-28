ddfc0bd2f10ec7f1db774c80efa694b38a1c0ac7
Ankit Thakur <ankit@wealthtech.in>
2023-09-28 13:44:54 +0530

    add reducer

--- src/components/model.ts
+++ src/components/model.ts
@@ -8,12 +8,24 @@ type Actions =
   | { type: "add"; payload: string }
   | { type: "remove"; payload: number }
   | { type: "done"; payload: number };
+
 import React, { useReducer } from "react";
 
-const TodoReducer = (state: Todo[], action) => {};
+const TodoReducer = (state: Todo[], action: Actions) => {
+  switch(action.type) {
+    case 'add':
+      return [
+        ...state,
+        {id: Date.now(), todo: action.payload, isDone: false}
+      ]
+  }
+};
+
 const ReducerExample = () => {
   const [state, dispatch] = useReducer(TodoReducer, []);
-  return <div></div>;
+  return (
+    <div/>
+  );
 };
 
 export default ReducerExample;
